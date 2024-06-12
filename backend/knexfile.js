767bd9348e019d05f4a0dd5d4897d2326e69511c
Wes Reid <bwreid@gmail.com>
2024-06-12 11:55:55 -0700

    Update to Knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -7,11 +7,8 @@
 require("dotenv").config();
 const path = require("path");
 
-const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  TEST_DATABASE_URL,
-  DEBUG,
-} = process.env;
+const { DATABASE_URL = "postgresql://postgres@localhost/postgres", DEBUG } =
+  process.env;
 
 module.exports = {
   development: {
@@ -19,7 +16,6 @@ module.exports = {
     pool: { min: 1, max: 5 },
     connection: {
       connectionString: DATABASE_URL,
-      ssl: true,
     },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
