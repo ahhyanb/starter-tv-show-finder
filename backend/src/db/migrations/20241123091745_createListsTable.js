3beebe9a30d2c5d129988f90d878af30f4ae5b96
Ryan Bartolome <rahhyahh@icloud.com>
2024-11-22 23:25:51 -1000

    added the create List table knex query

--- backend/src/db/migrations/20241123091745_createListsTable.js
+++ backend/src/db/migrations/20241123091745_createListsTable.js
@@ -3,7 +3,16 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("lists", (table) => {
+    table.increments("list_id").primary();
+    table.integer("account_id").unsigned().notNullable();
+    table 
+        .foreign("account_id")
+        .references("account_id")
+        .inTable("accounts")
+        .onDelete("CASCADE");
+    table.text("title").notNullable();
+  });
 };
 
 /**
@@ -11,5 +20,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable("lists");
 };
