1d22f5775a160e426b0c8e899a6911a0b885c15c
Ryan Bartolome <rahhyahh@icloud.com>
2024-11-25 16:14:19 -1000

    removed migration files

--- backend/src/lists/lists.service.js
+++ backend/src/lists/lists.service.js
@@ -1,21 +1,19 @@
 const knex = require("../db/connection");
 
-console.log(
-    knex("lists")
-      .select(
-        "lists.id as list_id",
-        "lists.title as list_title",
-        "lists.account_id",
-        "shows.id as show_id",
-        "shows.name as show_name",
-        "shows.genre as show_genre",
-        "shows.summary as show_summary"
-      )
-      .leftJoin("shows_lists", "lists.id", "shows_lists.list_id")
-      .leftJoin("shows", "shows_lists.show_id", "shows.id")
-      .toString()
-  );
-  
+function list() {
+  return knex("lists")
+    .select(
+      "lists.id as list_id",
+      "lists.title as list_title",
+      "lists.account_id",
+      "shows.id as show_id",
+      "shows.name as show_name",
+      "shows.genre as show_genre",
+      "shows.summary as show_summary"
+    )
+    .leftJoin("shows_lists", "lists.id", "shows_lists.list_id")
+    .leftJoin("shows", "shows_lists.show_id", "shows.id");
+}
 
 
 function create(newList) {
@@ -28,6 +26,6 @@ function create(newList) {
 
 
 module.exports = { 
-    // list,
+    list,
     create, 
 }
